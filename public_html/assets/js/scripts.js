59cff40900fd27c920c337a4765e64bb1708de39
oshoura <omarshoura1@gmail.com>
2022-12-31 18:08:44 -0500

    dark mode

--- public_html/assets/js/scripts.js
+++ public_html/assets/js/scripts.js
@@ -11,16 +11,13 @@ function openResume() {
 
 function getData(form) {
     name = form.elements['name_inp'].value;
-    email = form.elements['email_inp'].value;
     msg = form.elements['message_inp'].value;
 
-    console.log(name, email, msg)
+    console.log(name, msg)
 
     mail_request = "mailto:shoura.o@northeastern.edu?subject=Message from " + name + "&body=" + msg;
     window.open(mail_request);
-    // var formData = new FormData(form);
-    // console.log(document.getElementById("myForm").elements);
-    // console.log(Object.fromEntries(formData));
+
 }
   
 document.getElementById("myForm").addEventListener("submit", function (e) {
