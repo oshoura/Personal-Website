a673250ad3822c97ef2a2e8adab7a4c32cc799e9
oshoura <omarshoura1@gmail.com>
2023-01-05 21:07:12 -0500

    updated resume

--- public_html/assets/js/scripts.js
+++ public_html/assets/js/scripts.js
@@ -1,13 +1,3 @@
-function openResume() {
-    url = "../Resume 2022-Shoura.pdf"
-    // window.open(url);
-    const a = document.createElement('a')
-    a.href = url
-    a.download = url.split('/').pop()
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
-}
 
 function getData(form) {
     name = form.elements['name_inp'].value;
